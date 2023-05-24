0db20cfa7802c19cd93187a814e6591dd0556763
Hassan Alhaji <44787299+Hassan-Alhaji@users.noreply.github.com>
2023-05-24 18:47:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        StringBuilder sb = new StringBuilder("Hello World!!");
+
+        for (int i = 0; i < sb.Length; i++)
+            Console.Write(sb[i]);
+    }
+}
